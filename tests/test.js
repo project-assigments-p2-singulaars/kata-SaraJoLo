4ed1ecfc59234dcecb443d2b0abb21f3f1c6e51a
SaraJoLo <sarajorja.27@gmail.com>
2024-04-01 22:57:56 +0200

    testjss  a main

---
+++ tests/test.js
@@ -0,0 +1,15 @@
+
+
+function palabraPalidromo(cadena){
+  
+    let array = cadena.split("");
+   let reverse = array.reverse();
+
+    
+   return cadena == reverse.join("") ? "Siiii" : "No0o0o0"
+}
+
+
+console.log(palabraPalidromo("rama a amar"))
+
+document.write();
